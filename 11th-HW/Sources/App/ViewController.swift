a4dffaeffd6e815f507a8564e8237eb455bfd574
Maxim Soloboev <fateless@vk.com>
2022-09-01 21:44:37 +0500

    Setup project file.

---
+++ 11th-HW/Sources/App/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  11th-HW
+//
+//  Created by Максим Солобоев on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - Outlets
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupHierarchy()
+        setupLayout()
+    }
+
+    // MARK: - Setup
+
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    // MARK: - Actions
+}
+
